fab703bcdafcbbac79575ccb710ef561ede6e3b8
negin <negin.s.mousavi@gmail.com>
2021-09-28 12:00:17 +0330

    make array book as an app and has its main class to run

--- src/app/ArrayBook.java
+++ src/app/ArrayBook.java
@@ -4,6 +4,8 @@ import books.Book;
 import books.Fiction;
 import books.NonFiction;
 
+import java.util.Scanner;
+
 /**
  * @author Negin Mousavi
  */
@@ -11,6 +13,35 @@ public class ArrayBook {
 
     Book[] books = new Book[10];
     private int indexOfBooks = 0;
+
+    public static void main(String[] args) {
+        ArrayBook arrayBook = new ArrayBook();
+        String option;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Fiction or NonFiction?(f/n). to exit enter 'e': ");
+            option = scanner.nextLine().toLowerCase();
+            if (option.equals("e"))
+                break;
+            if (!option.equals("f") && !option.equals("n")) {
+                printInvalidInput();
+                continue;
+            }
+
+            System.out.print("Enter title: ");
+            String title = scanner.nextLine();
+
+            if (option.equals("f"))
+                arrayBook.createFictionBook(title);
+            else
+                arrayBook.createNonFictionBook(title);
+            System.out.println("your book added successfully :)");
+            arrayBook.showAllBooks();
+            printStar();
+        }
+    }
+
     public void createFictionBook(String title) {
         books[indexOfBooks++] = new Fiction(title);
     }
@@ -25,7 +56,15 @@ public class ArrayBook {
         ) {
             if (book == null)
                 break;
-            System.out.println(String.valueOf(book.getClass()).split("\\.")[1] + ": " + book.getTitle() + " " + book.getPrice());
+            System.out.println(book.toString());
         }
+    }
+
+    public static void printInvalidInput() {
+        System.out.println("invalid input");
+    }
+
+    public static void printStar() {
+        System.out.println("**********************************************************");
     }
 }
